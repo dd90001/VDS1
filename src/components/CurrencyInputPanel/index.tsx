6e12d015811434de32be67bebb641ba465e118c1
dd90001 <1292644504@qq.com>
2021-06-28 18:45:10 +0800

    23 commit

--- src/components/CurrencyInputPanel/index.tsx
+++ src/components/CurrencyInputPanel/index.tsx
@@ -230,7 +230,7 @@ export default function CurrencyInputPanel({
                     ? currency.symbol.slice(0, 4) +
                       '...' +
                       currency.symbol.slice(currency.symbol.length - 5, currency.symbol.length)
-                    : 'Vollar') || t('selectToken')}
+                    : currency?.symbol) }
                 </StyledTokenName>
               )}
               {!disableCurrencySelect && <StyledDropDown selected={!!currency} />}
