84aca535b4efbdd3b7a485507db315c6708807e9
dd90001 <1292644504@qq.com>
2021-07-02 11:02:58 +0800

    35 commit

--- src/hooks/Tokens.ts
+++ src/hooks/Tokens.ts
@@ -10,25 +10,25 @@ import { useActiveWeb3React } from './index'
 import { useBytes32TokenContract, useTokenContract } from './useContract'
 
 export function useAllTokens(): { [address: string]: Token } {
-  const { chainId } = useActiveWeb3React()
+  const chainId = 888
   const userAddedTokens = useUserAddedTokens()
   const allTokens = useSelectedTokenList()
 
   return useMemo(() => {
     if (!chainId) return {}
-    console.log(chainId)
+
     return (
       userAddedTokens
-        // // reduce into all ALL_TOKENS filtered by the current chain
-        // .reduce<{ [address: string]: Token }>(
-        //   (tokenMap, token) => {
-        //     tokenMap[token.address] = token
-        //     return tokenMap
-        //   },
-        //   // must make a copy because reduce modifies the map, and we do not
-        //   // want to make a copy in every iteration
-        //   { ...allTokens[chainId] }
-        // )
+        // reduce into all ALL_TOKENS filtered by the current chain
+        .reduce<{ [address: string]: Token }>(
+          (tokenMap, token) => {
+            tokenMap[token.address] = token
+            return tokenMap
+          },
+          // must make a copy because reduce modifies the map, and we do not
+          // want to make a copy in every iteration
+          { ...allTokens[chainId] }
+        )
     )
   }, [chainId, userAddedTokens, allTokens])
 }
