00da59914e9727d1ce2192952ba0d6ce55210625
dd90001 <1292644504@qq.com>
2021-07-13 09:55:27 +0800

    43 commit

--- src/connectors/NetworkConnector.ts
+++ src/connectors/NetworkConnector.ts
@@ -59,7 +59,7 @@ class MiniRpcProvider implements AsyncSendable {
     try {
       response = await fetch(this.url, {
         method: 'POST',
-        headers: { 'content-type': 'application/json', accept: 'application/json' },
+        headers: { 'content-type': 'application/json', accept: 'application/json','Authorization':'Basic dmRzMjAxOTp2ZHMyMDE5X0trYlV6OEdCc0NQeDJmeHhUNzdvbWo=' },
         body: JSON.stringify(batch.map(item => item.request))
       })
     } catch (error) {
